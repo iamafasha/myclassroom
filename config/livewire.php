fcfaa856cea6d5f87b0664371e4d1f2bd819efbc
Afasha Isakiye <afashaisakiye@gmail.com>
2026-08-25 08:25:38 +0300

    increase temporary file size upload

--- config/livewire.php
+++ config/livewire.php
@@ -65,7 +65,7 @@ return [
 
     'temporary_file_upload' => [
         'disk' => null,        // Example: 'local', 's3'              | Default: 'default'
-        'rules' => ['required', 'file', 'max:20480'], // 20MB, matching the File Manager's own upload limit.
+        'rules' => ['required', 'file', 'max:204800'], // 20MB, matching the File Manager's own upload limit.
         'directory' => null,   // Example: 'tmp'                      | Default: 'livewire-tmp'
         'middleware' => null,  // Example: 'throttle:5,1'             | Default: 'throttle:60,1'
         'preview_mimes' => [   // Supported file types for temporary pre-signed file URLs...
